de397759aef553bef02da8dd5ee1fc2a6928434f
JayJohnsonOIT <Jay.Johnson@oit.edu>
2021-01-31 05:15:46 -0800

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,5 +1,7 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 /**
  * Created by Jay Johnson 1/31/2021
  */
@@ -7,5 +9,6 @@ package edu.pcc.cis.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(new Date());
     }
 }
